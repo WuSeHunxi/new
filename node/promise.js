371e4ead7d58302b55e2cd3f32896c6ab8a47eb2
naxinyi <780957235@qq.com>
2019-12-17 13:07:32 +0800

    12.17

--- node/promise.js
+++ node/promise.js
@@ -27,10 +27,54 @@ var p1=new Promise(function(resolve,reject){
     })
 })
 
+var p2=new Promise(function(resolve,reject){
+    fs.readFile('./data/b.txt','utf8',function(err,data){
+        if(err){
+            // console.log(err);
+            reject(err);
+        }else{
+            resolve(data);
+        }
+    })
+})
 
+var p3=new Promise(function(resolve,reject){
+    fs.readFile('./data/c.txt','utf8',function(err,data){
+        if(err){
+            // console.log(err);
+            reject(err);
+        }else{
+            resolve(data);
+        }
+    })
+})
+
+//利用实例对象实现resolve和reject两个方法
 //当p1成功了，就then(then里面实现的就是resolve(data))
+// p1.then(function(data){
+//     console.log(data);
+// },function(err){//这个实现的就是reject(err)
+//     console.log("读取文件失败",err);
+// })
+
+
+
+//链式编程
+//如何解决嵌套问题
 p1.then(function(data){
     console.log(data);
+    return p2;
 },function(err){//这个实现的就是reject(err)
     console.log("读取文件失败",err);
+}).then(function(data){
+    //输出的是p2的数据
+    console.log(data);//前面那个then中return的是啥，这里的data就是啥，且后续的then就是p2的resolve()
+    return p3;
+}).then(function(data){
+    console.log(data);
+    console.log(data);
 })
+
+
+
+
