ebcf908c9a03f3e7e0d355d72d57f047478c9555
naxinyi <780957235@qq.com>
2019-12-17 16:49:28 +0800

    12.17

--- node/blog/app.js
+++ node/blog/app.js
@@ -1,5 +1,9 @@
-var express=require("express");
+/**
+ * npm i art-template express-art-template
+ */
 
+var express=require("express");
+var path=require("path");
 var app=express();
 
 /**
@@ -16,16 +20,27 @@ var app=express();
 /**
  * Node中的其他成员
  *      在每个模块中，除了require,exports等模块相关的API之外，还有两个特殊的成员：
- *             _dirname-->获取当前模块的(获取路径是动态的)
+ *             _dirname-->获取当前文件模块所属目录的绝对路径(获取路径是动态的)
  *             _filename-->获取当前文件的绝对路径(获取路径是动态的)
+ * 在文件中，相对路径是不可靠的，因为文件操作的路径被设计为node命令所处的路径中，为了解决该问题，需要把相对路径变为绝对路径，
+ * 需要使用_dirname和_filename
+ * 
+ * 
+ * 模块中的路径标识和文件操作中的相对路径标识不一致，
+ *      模块路径中的路径标识就是相对于当前文件模块，不受执行node命令所处路径影响
  */
 
 //path.join()-->将相对路径改成绝对路径
 app.use('/public/',express.static(path.join(_dirname,'./public/')));
-app.use('/node_modules/',express.static(path.join(_dirname,'public')));
+app.use('/node_modules/',express.static(path.join(_dirname,'./node_modules/')));
+
+app.engine('html',require('express-art-template'));
+app.set('views',path.join(_dirname,'./views/'));
 
 app.get('/',function(req,res){
-    res.send();
+    res.render('index.html',{
+        name:'哈根三'
+    })
 })
 
 app.listen(3000,function(){
--- node/blog/foo/index.js
+++ node/blog/foo/index.js
@@ -1,9 +1,12 @@
 var fs=require("fs");
+//通常情况下path和fs是成对引入的
 
 //这里面的./a.txt不是相对于当前执行文件的路径，而是相对于当前执行的node命令所处的路径
 //为了更好地运行，将相对路径改为绝对路径
 
-fs.readFile(__dirname+'/a.txt','utf8',function(err,data){
+
+//__dirname+'/a.txt' <---> path.join(__dirname,'/a.txt')
+fs.readFile(path.join(__dirname,'/a.txt'),'utf8',function(err,data){
     if(err){
         throw err;
     }
