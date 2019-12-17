30a633dc9a0a33c005aa8a4d0b6bd356487fa3f8
naxinyi <780957235@qq.com>
2019-12-17 12:22:02 +0800

    12.17

--- node/promise.js
+++ node/promise.js
@@ -1,1 +1,15 @@
+var fs=require('fs');
 console.log(1);
+
+new Promise(function(){
+    console.log(2);
+    fs.readFile('./data/a.txt','utf8',function(err,data){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(3+data);
+        }
+    })
+})
+
+console.log(4);
