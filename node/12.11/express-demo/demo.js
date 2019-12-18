68141477d266c24d495174ed7e931cd52f95331e
naxinyi <780957235@qq.com>
2019-12-18 09:58:39 +0800

    12.18

--- node/12.11/express-demo/demo.js
+++ node/12.11/express-demo/demo.js
@@ -5,11 +5,25 @@ app.use('/public/',express.static("./public/"));
 app.use('/static/',express.static("./public/"));
 
 app.get('/',function(req,res){
-
+        res.send("index.html");
 })
 
 app.get('/about',function(req,res){
-
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    
+    <body>
+        <h1>hello express</h1>
+    </body>
+    
+    </html>`);
 })
 
 app.listen(3000,function(){
