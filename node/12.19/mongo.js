1b171afed25f211e9c857e19e8c89cd9077b5741
naxinyi <780957235@qq.com>
2019-12-19 16:20:31 +0800

    12.19

--- node/12.19/mongo.js
+++ node/12.19/mongo.js
@@ -1,34 +1,70 @@
-var mongoose=require("mongoose");
-var Schema=mongoose.Schema;
-mongoose.connect('mongodb://localhost/student');
+// var mongoose=require("mongoose");
+// var Schema=mongoose.Schema;
+// mongoose.connect('mongodb://localhost/student');
 
-var studentSchema=new Schema({
+// var studentSchema=new Schema({
+//     name:{
+//         type:String,
+//         required:true
+//     },
+//     sex:{
+//         type:String,
+//         required:true
+//     },
+//     age:{
+//         type:String,
+//         required:true
+//     }
+// })
+
+// var Student=mongoose.model('Student',studentSchema);
+
+// var st1=new Student({
+//     name:"pap",
+//     sex:"男",
+//     age:10
+// })
+
+// st1.save(function(err,ret){
+//     if(err){
+//         console.log("保存失败");
+//     }else{
+//         console.log("保存成功"+ret);
+//     }
+// })
+
+
+
+
+var mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost/ST");
+var adSchema=new adSchema({
     name:{
         type:String,
         required:true
     },
     sex:{
         type:String,
-        required:true
+        enum:[-1,0,1],
+        default:-1
     },
     age:{
-        type:String,
-        required:true
+        type:String 
     }
 })
 
-var Student=mongoose.model('Student',studentSchema);
+var Admin=mongoose.model('Admin',adSchema);
 
-var st1=new Student({
-    name:"pap",
-    sex:"男",
+var admin=new Admin({
+    name:"apa",
+    sex:1,
     age:10
-})
+});
 
-st1.save(function(err,ret){
+admin.save(function(err,ret){
     if(err){
-        console.log("保存失败");
+        console.log(err)
     }else{
-        console.log("保存成功"+ret);
+        console.log(ret);
     }
 })
