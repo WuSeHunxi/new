a19d535fabd478a36167e30ab96ab0a1b58eb2c5
naxinyi <780957235@qq.com>
2020-02-25 15:50:06 +0800

    2.22

--- 2020/javascript/代码/2.25/轮播图/index.js
+++ 2020/javascript/代码/2.25/轮播图/index.js
@@ -1,7 +1,55 @@
-(function(){
+(function () {
 
-    var nowIndex=0;
-    var timer=null;
-    
+    var nowIndex = 0;
+    var timer = null;
+    var len = $('.item').length - 1;
+    var width = $('.item').width();
+
+    $('.lbtn').click(function () {
+        if (nowIndex == 0) {
+            $('.item').animate({
+                left: -width * len
+            })
+            nowIndex=len-1;
+        } else {
+            nowIndex--;     
+        }
+        change();
+    })
+
+    $('.rbtn').click(function () {
+        if(nowIndex==len){
+            $('.item').animate({
+                left:0
+            })
+            nowIndex=1;
+        }else{
+            nowIndex++;    
+        }
+        change();
+    })
+
+    $('.spots span').mouseenter(function () {
+        spotsChange();
+    })
+
+    $('.wrapper').mouseenter(function () {
+
+    })
+
+    $('.wrapper').mouseleave(function () {
+
+    })
+
+    function spotsChange() {
+        nowIndex = $(this).index();
+        $('.spots span').removeClass("active").eq(nowIndex).addClass("active");
+    }
+
+    function change() {
+        $('.list').animate({
+            left: nowIndex * width
+        })
+    }
 
 }())
