256883848673a31b8928c43b132f82bae8ae5be3
naxinyi <780957235@qq.com>
2020-02-26 07:07:40 +0800

    2.26

---
+++ 2020/javascript/代码/2.26‘/jd5/index.js
@@ -0,0 +1,16 @@
+$('#shortcut').load('./shortcut/index.html');
+// 同源的才可以进行网络请求
+// https://www.jd.com:443/ 
+// 
+$('#header').load('./header/index.html');
+$('#fs1').load('./fs/fs1.html');
+$('.fs3').load('./fs/fs3.html');
+$('.slider-list').swiper({
+    list: $('.slide-list-item'),
+    width: 190,
+    height: 470,
+    isAuto: true,
+    autoTime: 6000,
+    showChangeBtn: false,
+    showSpotBtn: false,
+})
