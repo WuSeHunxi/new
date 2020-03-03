cda9fef048eccbed8ff97269da12f744cb871889
naxinyi <780957235@qq.com>
2020-03-03 09:43:53 +0800

    3.3

--- 2020/javascript/代码/3.2/jd-self/index.js
+++ 2020/javascript/代码/3.2/jd-self/index.js
@@ -1,3 +1,7 @@
 
 $(".header").load('./header/index.html');
 $(".search").load('./searchContent/index.html');
+
+$('.c1').load('./content/c1.html');
+$('.c2').load('./content/c2.html');
+$('.c3').load('./content/c3.html');
