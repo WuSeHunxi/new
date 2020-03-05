c66695e338baa402553656aad3b7897737e6940e
naxinyi <780957235@qq.com>
2020-03-05 11:07:06 +0800

    3.5

---
+++ 2020/javascript/代码/3.2/jd-self/content/c2.js
@@ -0,0 +1,16 @@
+
+
+$('.content-swiper').swiper({
+    list:$('.content-swiper a'),
+    type:'animate',
+    isAuto:true,
+    showChangeBtn:true,
+    showSpotBtn:true,
+    autoTime:2000,
+    width:590,
+    height:470,
+    direction:'right'
+})
+
+
+
