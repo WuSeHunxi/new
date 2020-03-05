57b158374967fc2df7ad4d0c489fd028cd22dd81
naxinyi <780957235@qq.com>
2020-03-05 16:03:46 +0800

    3.2

---
+++ 2020/javascript/代码/3.5/boss直聘二级实战(1)/boss二级/js/demo.js
@@ -0,0 +1,50 @@
+(function(data){
+
+    function bindEvent(){
+        $('.citySelect').on('click',function(){
+            // console.log('e');
+            var provinceStr='';
+            // console.log(data);
+            data.arr.forEach(function(item,index){
+                console.log(item);
+                provinceStr+=`<li>${item}</li>`;
+            })
+            $('.province').html(provinceStr);
+            $('.dragDown').slideDown();
+        })
+
+        
+        $('.province').on('mouseenter','li',function(e){
+            var index=$(e.target).index();
+            $('.province li').css({
+                backgroundColor:"#fff"
+            }).eq($(e.target).index()).css({
+                backgroundColor:"#ddd"
+            })
+            var cityStr='';
+            cityArr=data.city[index];
+            // console.log(cityArr);
+            cityArr.forEach(function(item,index){
+                cityStr+=`<li>${item[1]}</li>`;
+            })
+            console.log(cityStr)
+            $('ul.city').html(cityArr);
+            $('#selectName').html($(this).html());
+        })
+
+        $('.province').on('click','li',function(){
+            $('#selectName').html($(this).html());
+            $('.dragDown').slideUp();
+        })
+
+        $('.dragDown').on('mouseleave',function(){
+            setTimeout(function(){
+                console.log(0);
+                $('.dragDown').slideUp();
+            },1000);
+        })
+    }
+
+    bindEvent()
+
+}(obj))
