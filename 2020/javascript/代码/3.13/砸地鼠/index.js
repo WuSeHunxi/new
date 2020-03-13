d262ac85094a63c6550b1c3f654dea50e3460750
naxinyi <780957235@qq.com>
2020-03-13 15:43:31 +0800

    3.13

---
+++ 2020/javascript/代码/3.13/砸地鼠/index.js
@@ -0,0 +1,9 @@
+var mouse={
+    el:document.getElementsByClassName("wrapper")[0],
+    scoreNode:document.getElementsByClassName("span")[0],
+    hole:document.getElementsByClassName("hole")[0],
+    holesList=document.getElementsByClassName("hole"),
+    length:'',
+    score:0,
+    
+}
