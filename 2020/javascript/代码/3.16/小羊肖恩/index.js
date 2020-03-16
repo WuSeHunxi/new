d98bde899c7b1182d794099805c892d698c38fbe
naxinyi <780957235@qq.com>
2020-03-16 08:05:57 +0800

    3.16

--- 2020/javascript/代码/3.16/小羊肖恩/index.js
+++ 2020/javascript/代码/3.16/小羊肖恩/index.js
@@ -1,7 +1,8 @@
 Sheep.prototype={
     init(){
         this.initData();
-        this.running()
+        this.createSheep()
+        this.running();
     },
     initData(){
         this.dom.className="sheep"
@@ -19,9 +20,49 @@ Sheep.prototype={
             if(self.dom.offsetLeft<=-164){
                 clearInterval(this.selfTimer);
                 self.dom.remove()
-                console.log(0)
+                // console.log(0)
             }
         },randomNum(0,this.frequence)+30);
+    },
+    createSheep(){
+        var self=this;
+        this.createTimer=setInterval(function(){
+            // console.log(self.wrapper.children)
+            if(self.wrapper.children.length<=5){
+                new Sheep({
+                    el:document.getElementsByClassName("wrapper")[0],
+                    speed:10,
+                    frequence:70,
+                    imgPosition:{
+                        x:0,
+                        y:0
+                    },
+                })
+                self.bindEvent();
+            }
+            
+        },1000);
+    },
+    bindEvent(){
+        var self=this;
+        this.dom.onmousedown=function(e){
+            var x=e.pageX;
+            var y=e.pageY;
+            self.speed=0;
+            self.imgY=-128;
+            document.onmousemove=function(e){
+                var chax=e.pageX-x;
+                var chay=e.pageY-y;
+                self.dom.style.left=chax+"px";
+                self.dom.style.top=chay+"px";
+            }
+
+            document.onmouseup=function(){
+                document.onmousemove=null;
+                self.speed=10;
+                self.imgY=0;
+            }
+        }
     }
 }
 
